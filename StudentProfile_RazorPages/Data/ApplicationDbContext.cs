571df4eb562914ff7ff96512f4d1141a22bf6d01
Promise30 <promiseadeagbo@gmail.com>
2023-02-16 18:11:34 +0100

    Added the Db Context File for the Project

---
+++ StudentProfile_RazorPages/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentProfile_RazorPages.Model;
+
+namespace StudentProfile_RazorPages.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student>StudentsTable { get; set; }
+    }
+}
