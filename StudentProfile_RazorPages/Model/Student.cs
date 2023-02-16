92670be8226efc3fe6a384ecfeb73ee64687bb8d
Promise30 <promiseadeagbo@gmail.com>
2023-02-16 18:12:11 +0100

    Created the Model class for the project

---
+++ StudentProfile_RazorPages/Model/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentProfile_RazorPages.Model
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+      
+        public int Age { get; set; }
+
+        [Range(100, 500, ErrorMessage ="Level should either be 100, 200, 300, 400, 500")]
+        [Required]
+        public int Level { get; set; }
+
+        [Required]
+        [Display(Name = "Course")]
+        public string course_of_study { get; set; }
+        
+
+    }
+}
